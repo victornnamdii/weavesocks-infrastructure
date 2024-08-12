74436bc4c745a0f04ae02cfaf6c8088dc6e7d022
“victornnamdii” <victorilodiuba@gmail.com>
2024-08-12 16:17:05 +0100

    minor fix

--- k8s/monitoring/modules/tools/grafana/main.tf
+++ k8s/monitoring/modules/tools/grafana/main.tf
@@ -1,17 +1,19 @@
 module "deployment" {
-  source = "./deployment"
-  namespace = var.namespace
+  source      = "./deployment"
+  namespace   = var.namespace
   secret_name = var.secret_name
 }
 
 module "service" {
-  source = "./service"
-  namespace = var.namespace
+  source             = "./service"
+  namespace          = var.namespace
   grafana_dep_labels = module.deployment.grafana_dep_labels
 }
 
 module "grafana_config" {
-  source = "./config"
-  namespace = var.namespace
+  source                 = "./config"
+  namespace              = var.namespace
   grafana_configmap_name = var.grafana_configmap_name
+
+  depends_on = [module.service]
 }
